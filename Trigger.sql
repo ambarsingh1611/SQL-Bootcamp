e05a26a67f5977bb6d96bbec3b02c196bf5a4df1
ambarsingh1611 <31308803+ambarsingh1611@users.noreply.github.com>
2019-12-30 14:29:19 -0700

    Add files via upload

---
+++ Trigger.sql
@@ -0,0 +1,16 @@
+/*-----------Triggers begin------------*/
+
+DELIMITER $$
+
+CREATE TRIGGER trig_hire_date
+BEFORE INSERT ON employees
+FOR EACH ROW
+BEGIN
+	IF NEW.hire_date > date_format(sysdate(), '%Y-%m-%d') THEN
+    SET NEW.hire_date = date_format(sysdate(), '%Y-%m-%d')
+    END IF;    
+END $$
+
+DELIMITER ;
+
+/*-----------Triggers end------------*/
